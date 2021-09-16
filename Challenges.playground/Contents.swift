62eeaba2ce3d165de90131eeeb9f32dab8e8ecb6
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-09-16 10:55:01 +0300

    Completed Challenge 1

--- Challenges.playground/Contents.swift
+++ Challenges.playground/Contents.swift
@@ -1,3 +1,20 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+// Unique characters (case sensitive)
+
+// MARK: - Solution
+func uniqueCharacters(input: String) -> Bool {
+	var characterSet = Set<Character>()
+	for character in input {
+		if characterSet.contains(character) { return false }
+		characterSet.insert(character)
+	}
+	return true
+}
+
+// MARK: - Tests
+assert(uniqueCharacters(input: "No duplicates") == true, "Unique characters challenge failed")
+assert(uniqueCharacters(input: "abcdefghijklmnopqrstuvwxyz") == true, "Unique characters challenge failed")
+assert(uniqueCharacters(input: "AaBbCc") == true, "Unique characters challenge failed")
+assert(uniqueCharacters(input: "Hello, world") == false, "Unique characters challenge failed")
+print("Success")
