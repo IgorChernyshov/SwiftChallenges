a41704e6a18b4ad10bbf89e9be1482ce5ee7cc42
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-09-20 10:35:02 +0300

    Added a regex solution of Challenge 7

--- Challenges.playground/Pages/Challenge 7.xcplaygroundpage/Contents.swift
+++ Challenges.playground/Pages/Challenge 7.xcplaygroundpage/Contents.swift
@@ -11,6 +11,12 @@ extension String {
 			$0.append($1)
 		}
 	}
+
+	func regexCondensingWhitespaces() -> String {
+		// Regex are about 50% slower, but the intense is more clear
+		// Find " +" ("one or more spaces") and replace them with " "
+		replacingOccurrences(of: " +", with: " ", options: .regularExpression, range: nil)
+	}
 }
 
 // MARK: - Tests
@@ -18,3 +24,8 @@ extension String {
 "    a".condensingWhitespaces() == " a"
 "abc".condensingWhitespaces() == "abc"
 "a b".condensingWhitespaces() == "a b"
+// -----
+"a   b   c".regexCondensingWhitespaces() == "a b c"
+"    a".regexCondensingWhitespaces() == " a"
+"abc".regexCondensingWhitespaces() == "abc"
+"a b".regexCondensingWhitespaces() == "a b"
