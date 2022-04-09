5657715134960ae43ed14ef134a384e42d56964a
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2022-04-09 16:01:45 +0300

    Completed Challenge 37

---
+++ Challenges.playground/Pages/Challenge 37.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+// Count the numbers
+// Write an extension for collections of integers that returns the number of times
+// a specific digit appears in any of its numbers.
+
+
+// MARK: - Solution
+extension Collection where Iterator.Element == Int {
+
+	func count(digit: String) -> Int {
+		self.reduce(into: "") { $0 += $1.description }.filter { $0 == Character(digit) }.count
+	}
+}
+
+// MARK: - Tests
+[5, 15, 55, 515].count(digit: "5") == 6
+[5, 15, 55, 515].count(digit: "1") == 2
+[55555].count(digit: "5") == 5
+[55555].count(digit: "1") == 0
