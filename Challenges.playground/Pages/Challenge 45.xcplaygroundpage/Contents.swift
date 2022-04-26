d16fe7012745ac98bf1a162e980afdc813e5d976
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2022-04-26 19:40:28 +0300

    Added Challenge 45 Boilerplate

---
+++ Challenges.playground/Pages/Challenge 45.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+// Traversing the tree
+// Note: this challenge cannot be attempted until you have first completed challenge 54.
+// Write a new method for your binary search tree that traverses the tree in order,
+// running a closure on each node.
+// Tip: Traversing a node in order means visiting its left value,
+// then visiting its own value, then visiting its right value.
+
+// MARK: - Solution
+
+// MARK: - Tests
